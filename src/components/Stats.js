7ff72cf9bd4df6adaa72ee3411efaa40ce892b03
Dhanush <dhanushpgowda73@gmail.com>
2025-07-23 11:10:11 +0530

    Add emoji to prompt for adding items in Stats component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,7 +2,7 @@ export default function Stats({ items }) {
   if (!items.length)
     return (
       <p className="stats">
-        <em>Start adding some items to your packing list</em>
+        <em>Start adding some items to your packing list 🚀</em>
       </p>
     );
 
